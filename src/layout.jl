d485f19e6845cf034bfaf1366b9306243bdc84ad
Anton Hinneck <37851720+antonhinneck@users.noreply.github.com>
2019-06-11 15:28:44 +0300

    version 0.2

--- src/layout.jl
+++ src/layout.jl
@@ -7,6 +7,18 @@ function build_layout(assets)
 
     vertices = [i for i in 1:length(assets)]
 
+    @inline function get_M(assets)
+        sum_x = 0
+        sum_y = 0
+        for i in 1:length(assets)
+            sum_x += assets[i][1]
+            sum_y += assets[i][2]
+        end
+        return maximum([sum_x, sum_y])
+    end
+
+    M = get_M(assets)
+
     buildLayout = Model()
 
     @variable(buildLayout, pos_x[vertices] >= 0)
@@ -14,12 +26,12 @@ function build_layout(assets)
     @variable(buildLayout, max_x >= 0)
     @variable(buildLayout, max_y >= 0)
     @variable(buildLayout, collsion_x[vertices, vertices], Bin)
-    # 1: First element displayed at a lower x-index
-    # 2: Second element displayed at a lower x-index
+    # 0: First element displayed at a lower x-index
+    # 1: Second element displayed at a lower x-index
     #-----------------------------------------------
     @variable(buildLayout, collsion_y[vertices, vertices], Bin)
-    # 1: First element displayed at a lower y-index
-    # 2: Second element displayed at a lower y-index
+    # 0: First element displayed at a lower y-index
+    # 1: Second element displayed at a lower y-index
     #-----------------------------------------------
     @variable(buildLayout, adj_angle[vertices, vertices, 4], Bin)
 
@@ -31,14 +43,17 @@ function build_layout(assets)
     @constraint(buildLayout, set_max_y[v = vertices],
     pos_y[v] <= max_y)
 
-    @constraint(buildLayout, collision_x_1[v1 = vertices, v2 = vertices],
-    pos_x[v1] + assets[v1][1] <= pos_x[v2])
+    @constraint(buildLayout, collision_x_1[v1 = vertices, v2 = vertices; v1 != v2],
+    pos_x[v1] + assets[v1][1] <= pos_x[v2] + collision_x[v1, v2] * M)
 
-    @constraint(buildLayout, collision_x_2[v1 = vertices, v2 = vertices],
-    pos_x[v1] + assets[v1][1] <= pos_x[v2])
+    @constraint(buildLayout, collision_x_2[v1 = vertices, v2 = vertices; v1 != v2],
+    pos_x[v1] - assets[v2][1] * (1 - collision_x[v1,v2]) * M >= pos_x[v2])
 
-    @constraint(buildLayout, set_max_y[v1 = vertices, v2 = vertices],
-    pos_x[v1] <= pos_x[v2])
+    @constraint(buildLayout, collision_y_1[v1 = vertices, v2 = vertices; v1 != v2],
+    pos_y[v1] + assets[v1][1] <= pos_y[v2] + collision_y[v1, v2] * M)
+
+    @constraint(buildLayout, collision_y_2[v1 = vertices, v2 = vertices; v1 != v2],
+    pos_y[v1] - assets[v2][1] * (1 - collision_y[v1,v2]) * M >= pos_y[v2])
 
     status = optimize!(TS, with_optimizer(Gurobi.Optimizer, OutputFlag = 0))
 
