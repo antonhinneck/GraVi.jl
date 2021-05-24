6785bde2c0e1e24cee9b3267be1c21f8b5e7d334
Anton Hinneck <37851720+antonhinneck@users.noreply.github.com>
2021-05-25 00:45:55 +0300

    Update test.jl

--- test/test.jl
+++ test/test.jl
@@ -1,4 +1,13 @@
-include("C:/Users/Anton Hinneck/.julia/packages/GraphVisualization/src/GraphVisualization.jl")
-include("C:/Users/Anton Hinneck/.julia/packages/PowerGrids/src/PowerGrids.jl")
+include("C:/Users/Anton Hinneck/Desktop/Libya/GraphVisualization.jl/src/GraphVisualization.jl")
+include("C:/Users/Anton Hinneck/juliaPackages/GitHub/PowerGrids.jl/src/PowerGrids.jl")
+using .PowerGrids, .GraphVisualization
+dir = @__DIR__
+cd(dir)
 
-grid = PowerGrids.readDataset(PowerGrids.datasets()[5])
+set_csv_path("C:/Users/Anton Hinneck/Documents/Git/pglib2csv/pglib/2020-08-21.19-54-30-275/csv")
+PowerGrids.csv_cases(verbose = true)
+PowerGrids.select_csv_case(48)
+case = PowerGrids.loadCase() # 118 Bus ieee
+
+graph = PowerGrids.toGraph(case)
+GraphVisualization.plot(graph, [600, 600]; export_dir = dir)
